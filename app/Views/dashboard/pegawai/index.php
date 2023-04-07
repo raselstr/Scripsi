7bc3e96437d895352e61e016be248333f764a0fd
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2023-04-07 15:52:51 +0700

    Modal berhasil

--- app/Views/dashboard/pegawai/index.php
+++ app/Views/dashboard/pegawai/index.php
@@ -17,8 +17,8 @@
           <?= esc($title); ?>
         </div>
         <div class="card-body">
-        <button type="button" class="btn btn-primary mb-2" data-toggle="modal" data-target="#tambahModal">
-          Launch demo modal
+        <button type="button" class="btn btn-primary mb-2" data-bs-toggle="modal" data-bs-target="#tambahModal">
+          Tambah Data Pegawai
         </button>       
         
           <table id="datatablesSimple">
@@ -56,20 +56,18 @@
   
 
   <!-- Modal -->
-  <div class="modal fade" id="tambahModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+  <div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           ...
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
           <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
